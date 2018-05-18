5edb4202da35fe35f4ff34081e7a92621ff822da
Jadimo <jadimo007@gmail.com>
2018-05-18 21:02:20 +0200

    Try at androdi app

---
+++ app/src/main/java/com/jadimo/testapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.jadimo.testapp;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PackageManager pm = getPackageManager();
+        if (pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            ToastTest();
+        }
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    public void ToastTest() {
+        Toast.makeText(getApplicationContext(),"NFC Obecne!",Toast.LENGTH_SHORT).show();
+    }
+
+}
