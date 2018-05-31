daf7f0e992f48dcaaaf04e0b81b43fd3f5f09f20
Jadimo <jadimo007@gmail.com>
2018-05-31 20:24:58 +0200

    Cleanups

--- app/src/main/java/com/jadimo/nfcib/MainActivity.java
+++ app/src/main/java/com/jadimo/nfcib/MainActivity.java
@@ -41,6 +41,90 @@ public class MainActivity extends Activity {
     ImageView Status;
 
     public void Send(View v) {
+
+        nfcaction();
+    final Handler delay = new Handler();
+        delay.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            scan.setIndeterminate(true);
+            scan.setProgress(50);
+            tvNFCContent.setText(R.string.Waiting);
+            Status.setImageResource(R.drawable.wait);
+        }
+    },3500);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+
+        tvNFCContent = findViewById(R.id.nfc_contents);
+        btnWrite = findViewById(R.id.button);
+        ID = findViewById(R.id.IDdisplay);
+        ID2 = findViewById(R.id.ID2);
+        scan = findViewById(R.id.scanning);
+        Status = findViewById(R.id.info);
+
+        android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        ID2.setText(R.string.UniqueID);
+        ID.setText(android_id);
+        tvNFCContent.setText(R.string.Waiting);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(MainActivity.this);
+        if (nfcAdapter == null) {
+            tvNFCContent.setText(R.string.NO_NFC);
+            finish();
+        }
+
+        if (!nfcAdapter.isEnabled()){
+            tvNFCContent.setText(R.string.NFC_OFF);
+        }
+        else {
+            HandleNFCIntent(getIntent());
+        }
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagDetected, ndefDetected, techDetected };
+    }
+
+    private void HandleNFCIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            nfcaction();
+        }
+        final Handler delay = new Handler();
+        delay.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scan.setIndeterminate(true);
+                scan.setProgress(50);
+                tvNFCContent.setText(R.string.Waiting);
+                Status.setImageResource(R.drawable.wait);
+            }
+        },3500);
+    }
+
+    private void write(Tag tag) throws IOException, FormatException {
+        String record = "news:" + android_id + "\0";
+        NdefRecord recordNFC;
+        recordNFC = NdefRecord.createUri(record);
+        NdefMessage message = new NdefMessage(recordNFC);
+        Ndef ndef = Ndef.get(tag);
+        ndef.connect();
+        ndef.writeNdefMessage(message);
+        ndef.close();
+    }
+
+    private void nfcaction(){
         try {
             myTag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
             write(myTag);
@@ -67,126 +151,16 @@ public class MainActivity extends Activity {
             Toast.makeText(context, R.string.NO_TAG, Toast.LENGTH_LONG).show();
             e.printStackTrace();
             tvNFCContent.setText(R.string.Tag_error);
-            Status.setImageResource(R.drawable.wait);
-            scan.setIndeterminate(false);
-            scan.setProgress(0);
-        }
-    final Handler delay = new Handler();
-        delay.postDelayed(new Runnable() {
-        @Override
-        public void run() {
-            scan.setIndeterminate(true);
-            scan.setProgress(50);
-            tvNFCContent.setText(R.string.Waiting);
-        }
-    },3000);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        context = this;
-
-        tvNFCContent = findViewById(R.id.nfc_contents);
-        btnWrite = findViewById(R.id.button);
-        ID = findViewById(R.id.IDdisplay);
-        ID2 = findViewById(R.id.ID2);
-        scan = findViewById(R.id.scanning);
-        Status = findViewById(R.id.info);
-
-        android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-        ID2.setText(R.string.UniqueID);
-        ID.setText(android_id);
-        tvNFCContent.setText(R.string.Waiting);
-
-        nfcAdapter = NfcAdapter.getDefaultAdapter(MainActivity.this);
-        if (nfcAdapter == null) {
-            tvNFCContent.setText(R.string.NO_NFC);
-            scan.setIndeterminate(false);
-            scan.setProgress(0);
             Status.setImageResource(R.drawable.error);
+            scan.setIndeterminate(false);
+            scan.setProgress(0);
         }
-
-        if (!nfcAdapter.isEnabled()){
-            tvNFCContent.setText(R.string.NFC_OFF);
-        }
-        else {
-            readFromIntent(getIntent());
-        }
-
-        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
-        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-        writeTagFilters = new IntentFilter[] { tagDetected, ndefDetected, techDetected };
-    }
-
-    private void readFromIntent(Intent intent) {
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-
-            try {
-                myTag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                write(myTag);
-                Toast.makeText(context, R.string.WRITE_SUCCESS, Toast.LENGTH_LONG).show();
-                Status.setImageResource(R.drawable.success);
-                tvNFCContent.setText(R.string.Success);
-                scan.setIndeterminate(false);
-                scan.setProgress(100);
-            } catch (IOException e) {
-                Toast.makeText(context, R.string.WRITE_ERROR, Toast.LENGTH_LONG ).show();
-                e.printStackTrace();
-                tvNFCContent.setText(R.string.Error);
-                Status.setImageResource(R.drawable.error);
-                scan.setIndeterminate(false);
-                scan.setProgress(0);
-            } catch (FormatException e) {
-                Toast.makeText(context, R.string.WRITE_ERROR, Toast.LENGTH_LONG ).show();
-                e.printStackTrace();
-                tvNFCContent.setText(R.string.Error);
-                Status.setImageResource(R.drawable.error);
-                scan.setIndeterminate(false);
-                scan.setProgress(0);
-            } catch (NullPointerException e) {
-                Toast.makeText(context, R.string.NO_TAG, Toast.LENGTH_LONG).show();
-                e.printStackTrace();
-                tvNFCContent.setText(R.string.Tag_error);
-                Status.setImageResource(R.drawable.error);
-                scan.setIndeterminate(false);
-                scan.setProgress(0);
-            }
-        }
-        final Handler delay = new Handler();
-        delay.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                scan.setIndeterminate(true);
-                scan.setProgress(50);
-                tvNFCContent.setText(R.string.Waiting);
-                Status.setImageResource(R.drawable.wait);
-            }
-        },2000);
-    }
-
-    private void write(Tag tag) throws IOException, FormatException {
-        String record = "news:" + android_id + "\0";
-        NdefRecord recordNFC;
-        recordNFC = NdefRecord.createUri(record);
-        NdefMessage message = new NdefMessage(recordNFC);
-        Ndef ndef = Ndef.get(tag);
-        ndef.connect();
-        ndef.writeNdefMessage(message);
-        ndef.close();
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         setIntent(intent);
-        readFromIntent(intent);
+        HandleNFCIntent(intent);
     }
 
     @Override
@@ -199,6 +173,14 @@ public class MainActivity extends Activity {
     public void onResume(){
         super.onResume();
         WriteModeOn();
+    }
+
+    @Override
+    public  void onDestroy(){
+        super.onDestroy();
+        scan.setIndeterminate(false);
+        scan.setProgress(0);
+        Status.setImageResource(R.drawable.error);
     }
 
     private void WriteModeOn(){
@@ -216,6 +198,3 @@ public class MainActivity extends Activity {
             startActivity(i);
     }
 }
-
-
-
